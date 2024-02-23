8438722d95d3aa62419f3516d67f0a13c17579de
Bharat Kathi <bharat1031@gmail.com>
2024-02-22 20:06:45 -0800

    lab4b updates

--- lab4/L4B/src/UART.c
+++ lab4/L4B/src/UART.c
@@ -144,7 +144,7 @@ void USART_Write(USART_TypeDef * USARTx, uint8_t *buffer, uint32_t nBytes) {
 		while (!(USARTx->ISR & USART_ISR_TXE));   	// wait until TXE (TX empty) bit is set
 		// Writing USART_DR automatically clears the TXE flag 	
 		USARTx->TDR = buffer[i] & 0xFF;
-		USART_Delay(40000); // Increase from 300 to 40000 for bluetooth
+		USART_Delay(300); // Increase from 300 to 40000 for bluetooth
 	}
 	while (!(USARTx->ISR & USART_ISR_TC));   		  // wait until TC bit is set
 	USARTx->ISR &= ~USART_ISR_TC;
