f9725e6aea01008a8e8f950cd5142d004e71a163
Bharat Kathi <bharat1031@gmail.com>
2024-03-01 15:50:33 -0800

    update lab5

--- lab5/L5A/src/SysClock.c
+++ lab5/L5A/src/SysClock.c
@@ -1,12 +1,21 @@
 #include "SysClock.h"
 
 void System_Clock_Init(void) {
-	// Enable HSI
-	RCC->CR |= RCC_CR_HSION;
-	while((RCC->CR & RCC_CR_HSIRDY) == 0);
-	
-	// Select HSI as system clock source
+	// Select MSI as system clock source (8 MHz)
 	RCC->CFGR &= ~RCC_CFGR_SW;
-	RCC->CFGR |= RCC_CFGR_SW_HSI;
-	while((RCC->CFGR & RCC_CFGR_SWS) == 0);
+	RCC->CFGR |= RCC_CFGR_SW_MSI;
+	
+	// Set MSI clock range
+	while((RCC->CR & RCC_CR_MSIRDY) == 0);
+	RCC->CR &= ~RCC_CR_MSION; // MSI off to modify MSIRANGE
+	RCC->CR &= ~RCC_CR_MSIRANGE;
+	RCC->CR |= RCC_CR_MSIRANGE_7; // 8 MHz
+	// Use the MSI clock range that is defined in RCC_CR
+	RCC->CR |= RCC_CR_MSIRGSEL;
+	// Enable MSI oscillator
+	RCC->CR |= RCC_CR_MSION;
+	// Wait until MSI is ready
+	while(!(RCC->CR & RCC_CR_MSIRDY));
+	// System clock switch status set by hardware to MSI (00)
+	while(RCC->CFGR & RCC_CFGR_SWS);
 }
